b263b190c11e80868e06b73e49fdf37cb3ba4fb5
Sachin-Phunage <phunage.sachin@gmail.com>
2025-12-08 16:52:09 +0530

    Stream Find methods example

--- src/main/java/org/streams/terminal/StreamFindElement.java
+++ src/main/java/org/streams/terminal/StreamFindElement.java
@@ -7,15 +7,16 @@ import java.util.Optional;
 public class StreamFindElement {
     public static void main(String[] args) {
 
-        // FInd first element satisfying the given condition
         List<Integer> intList = Arrays.asList(4,11,8,10,14);
+
+        // Find first element satisfying the given condition
         Optional<Integer> findFirst = intList.stream()
                 .filter(x -> x % 5 == 0)
                 .findFirst();
         if(findFirst.isPresent()){
             System.out.println("Find first result = "+findFirst.get());
         } else {
-            System.out.println("No element found");
+            System.out.println("No element found for Find first");
         }
 
         //When no condition given it will return first element of Stream
@@ -24,7 +25,26 @@ public class StreamFindElement {
         if(findFirstNoCondition.isPresent()){
             System.out.println("Find first result when No condition given = "+findFirstNoCondition.get());
         } else {
-            System.out.println("No element found");
+            System.out.println("No element found for Find First");
+        }
+
+        // Find Any with filter condition
+        Optional<Integer> findAnyWithConditionInt = intList
+                .stream()
+                .filter(x->x%2!=0)
+                .findAny();
+        if(findAnyWithConditionInt.isPresent()){
+            System.out.println("Find any result when condition given = "+findAnyWithConditionInt.get());
+        } else {
+            System.out.println("No element found for Find any with condition");
+        }
+
+        // Find Any with No filter condition
+        Optional<Integer> findAnyInt = intList.stream().findAny();
+        if(findAnyInt.isPresent()){
+            System.out.println("Find any result when No condition given = "+findAnyInt.get());
+        } else {
+            System.out.println("No element found for Find any without condition");
         }
     }
 }
